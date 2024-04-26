b945841b8cd6f047dc31762a6ed657af220ed7d4
BigM19 <nmatedan@gmail.com>
2024-04-26 10:08:49 +0200

    Adatbáziskezelő modellek létrehozása

---
+++ ShirtDesigner/Models/DesignableProduct.cs
@@ -0,0 +1,42 @@
+﻿/*
+' Copyright (c) 2024 bakaJzspiG
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using DotNetNuke.Entities.Content;
+using System;
+using System.Web.Caching;
+using System.Web.UI.WebControls;
+
+namespace bakaJzspiG.ShirtDesigner.ShirtDesigner.Models
+{
+    [TableName("DesignableProduct")]
+    [PrimaryKey("ItemId", AutoIncrement = true)]
+    [Cacheable("DesignableProduct", CacheItemPriority.Default, 20)]
+    [Scope("ModuleId")]
+    public class DesignableProduct
+    {
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public string ItemSize { get; set; }
+
+        public int ItemPrice { get; set; }
+
+        public string ItemPic { get; set; }
+
+    }
+        
+}
