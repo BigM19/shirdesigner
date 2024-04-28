c71cb6eb52ed147b7cbd8aa8242914fdfc3b1461
BigM19 <nmatedan@gmail.com>
2024-04-29 01:50:05 +0200

    Detail Kontroller és view létrehozása

--- Designer/Controllers/DesignableProductController.cs
+++ Designer/Controllers/DesignableProductController.cs
@@ -29,46 +29,10 @@ namespace Designer.Dnn.Designer.Controllers
         [HttpGet]
         public ActionResult Detail(int id)
         {
-            DesignableProduct product = null;
-            string connectionString = "Data Source=NAGYMATE\\SQLEXPRESS;Initial Catalog=MyDNNDatabase;Integrated Security=True"; // Update this with your actual connection string
-            string query = "SELECT * FROM DesignableProducts WHERE ItemId = @ItemId";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@ItemId", id);
-
-                try
-                {
-                    connection.Open();
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.Read())
-                    {
-                        product = new DesignableProduct
-                        {
-                            ItemId = (int)reader["ItemId"],
-                            ItemName = reader["ItemName"].ToString(),
-                            ItemDescription = reader["ItemDescription"].ToString(),
-                            ItemSize = reader["ItemSize"].ToString(),
-                            ItemPrice = (int)reader["ItemPrice"],
-                            ItemPic = reader["ItemPic"].ToString()
-                        };
-                    }
-                    reader.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return new HttpStatusCodeResult(500, "Internal Server Error"); // Return a 500 error in case of exception
-                }
-            }
-
-            if (product == null)
-            {
-                return HttpNotFound(); // Returns a 404 error if no product is found
-            }
-
+            DesignableProduct product;
+            IDataContext ctx = DataContext.Instance();
+            var rep = ctx.GetRepository<DesignableProduct>();
+            product = rep.GetById(id);
             return View(product);
         }
 
