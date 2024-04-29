7e9847d9a3c0140f2fe52182cf83cbd72dd1ffda
BigM19 <nmatedan@gmail.com>
2024-04-30 01:20:53 +0200

    Kép feltöltő létrehozva

--- Designer/Controllers/DesignableProductController.cs
+++ Designer/Controllers/DesignableProductController.cs
@@ -9,26 +9,30 @@ using Hotcakes.Commerce.Catalog;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
 using System.Web.Mvc;
+using System.Windows.Forms;
 
 namespace Designer.Dnn.Designer.Controllers
 {
     public class DesignableProductController : DnnController
     {
+        public int itemId;
         public ActionResult Index()
         {
             IDataContext ctx = DataContext.Instance();
             var rep = ctx.GetRepository<DesignableProduct>();
-            var products = rep
-                .Find("")
-                .ToArray();
+            var products = rep.Get();
             return View(products);
         }
 
         [HttpGet]
         public ActionResult Detail(int id)
         {
+            itemId = id;
             DesignableProduct product;
             IDataContext ctx = DataContext.Instance();
             var rep = ctx.GetRepository<DesignableProduct>();
@@ -36,5 +40,57 @@ namespace Designer.Dnn.Designer.Controllers
             return View(product);
         }
 
+        [HttpPost]
+        public ActionResult Detail(HttpPostedFileBase graphicFile)
+        {
+            DesignableProduct product;
+            IDataContext ctx = DataContext.Instance();
+
+            if (graphicFile != null && graphicFile.ContentLength > 0)
+            {
+                var rep2 = ctx.GetRepository<UsableGraphics>();
+                //int maxId = rep2.Get().Max(g => (int?)g.GraphicId) ?? 0; // Use nullable int to handle an empty table
+                //int newId = maxId + 1;
+
+                var fileName = Path.GetFileName(graphicFile.FileName);
+
+                var path = Path.Combine(@"C:\DNN\DesktopModules\MVC\shirtdesigner\Designer\Assets\UsableGraphics\", fileName);
+
+                graphicFile.SaveAs(path);
+
+                UsableGraphics graphic = new UsableGraphics
+                {
+                    //GraphicId = newId,
+                    GraphicPic = fileName
+                };
+
+                try
+                {
+                    rep2.Insert(graphic);
+                    ctx.Commit();
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = ex.Message;
+                }
+
+            }
+            
+            var rep = ctx.GetRepository<DesignableProduct>();
+            product = rep.GetById(itemId);
+            
+            return View(product);
+
+        }
+
+        [HttpGet]
+        public ActionResult GetUsableGraphics()
+        {
+            IDataContext ctx = DataContext.Instance();
+            var rep = ctx.GetRepository<UsableGraphics>();
+            var images = rep.Get();
+
+            return PartialView("GetUsableGraphics",images);
+        }
     }
 }
